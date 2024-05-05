6c01cda36cab22f8cd1529a430e93380de5287c3
Saurabh Bakolia <78996216+saurabhbakolia@users.noreply.github.com>
2024-05-05 11:52:42 +0530

    initial commit

---
+++ src/components/add-transaction/index.jsx
@@ -0,0 +1,44 @@
+import { Button, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Radio, RadioGroup } from '@chakra-ui/react'
+import React from 'react'
+
+const TransactionForm = ({ onClose, isOpen }) => {
+    return (
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <form>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Add New Transaction</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <FormControl>
+                            <FormLabel>Enter Description</FormLabel>
+                            <Input
+                                placeholder="Enter Transaction description"
+                                name="description"
+                                type="text"
+                            />
+                        </FormControl>
+                        <FormControl>
+                            <FormLabel>Enter Amount</FormLabel>
+                            <Input
+                                placeholder="Enter Transaction amount"
+                                name="amount"
+                                type="number"
+                            />
+                        </FormControl>
+                        <RadioGroup mt={"5"}>
+                            <Radio value='income' colorScheme='blue' name='type'>Expense</Radio>
+                            <Radio value="expense" colorScheme='red' name='type'>Income</Radio>
+                        </RadioGroup>
+                    </ModalBody>
+                    <ModalFooter>
+                        <Button mr={"4"} onClick={onClose}>Cancel</Button>
+                        <Button>Add</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </form>
+        </Modal>
+    )
+}
+
+export default TransactionForm
